3f72a25eb094ddce6e8ae178053d4f9700d33bb2
Caue Almeida <nncl@live.com>
2024-04-15 23:13:02 -0300

    test(spies): Creates spy
    Using simon and mocked data

--- modules/03-spies/src/fibonacci.js
+++ modules/03-spies/src/fibonacci.js
@@ -4,7 +4,14 @@ class Fibonacci {
    * 0,1,1,2,3,5,8,13,21
    */
 
-  execute(current, index, next) {
+  * execute(input, current = 0, next = 1) {
+    if (!input) {
+      return
+    }
+
+    yield current
+
+    yield* this.execute(input - 1, next, current + next);
   }
 }
 
--- modules/03-spies/src/fibonacci.test.js
+++ modules/03-spies/src/fibonacci.test.js
@@ -1,10 +1,55 @@
 const assert = require('assert');
-const Fibonacci = require('./fibonacci')
+const { createSandbox } = require("sinon");
+const Fibonacci = require('./fibonacci');
+const sinon = createSandbox();
 
-;(() => {
+
+(() => {
+
+  /*
+   * 0,1,1,2,3
+   */
+  {
+
+    const fibonacci = new Fibonacci();
+    const spy = sinon.spy(
+      fibonacci,
+      fibonacci.execute.name
+    )
+
+    for (const sequence of fibonacci.execute(3)) {
+    }
+
+    const expectedCallCount = 4;
+    assert.strictEqual(spy.callCount, expectedCallCount);
+  }
+
   {
     const fibonacci = new Fibonacci();
-    const result = fibonacci.execute();
-    // assert.deepEqual(1, 1);
+    const spy = sinon.spy(
+      fibonacci,
+      fibonacci.execute.name
+    )
+
+    const results = [...fibonacci.execute(5)];
+
+    const expectedCallCount = 6;
+    assert.strictEqual(spy.callCount, expectedCallCount);
+
+    const { args } = spy.getCall(2);
+    const expectedParams = [2,2,3]; // 3,1,2
+    /*
+     * 5 sequences (input - 1, current, current + next)
+     * [0] = input = 5, current = 0, next = 1 : result 0
+     * [1] = input = 4, current = 1, next = 1 : result 1
+     * [2] = input = 3, current = 1, next = 2 : result 1
+     * [3] = input = 2, current = 2, next = 3 : result 1
+     * [4] = input = 1, current = 3, next = 5 : result 1
+     * [5] = input = 0, current = 5, next = 8 : STOP
+     */
+    assert.deepStrictEqual(args, expectedParams, "Arrays must be equal")
+
+    const expectedResults = [0,1,1,2,3];
+    assert.deepStrictEqual(results, expectedResults, "Arrays must be equal")
   }
 })();
